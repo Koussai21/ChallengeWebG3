0243f4130943feabdb50fb917ed6e10e4a86dc4b
Koukou21 <koussailaribi21@gmail.com>
2024-01-08 09:18:53 +0100

    new file:   Efreipedia/view/authentification.php 	modified:   Efreipedia/view/header.php 	modified:   Efreipedia/view/header.php

--- Efreipedia/view/header.php
+++ Efreipedia/view/header.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Site Web</title>
-    <style>
-        header{
-            border: solid;
-        }
-    </style>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Alpine.js CDN -->
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+  <!-- TailwindCSS CDN -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.x.x/dist/tailwind.min.css">
+  <title>Formation alpine.js | Use API</title>
 </head>
 
-<header>
+<header class="solid">
     <h1>Mon Site Web</h1>
     <a href="/inscription">S'inscrire</a>
     <a href="index.php?page=accueil">Accueil</a>
     <a href="index.php?page=article">Nos articles</a>
+    <a href="index.php?page=connexion">Se connecter</a>
 </header>
 
 <body>
